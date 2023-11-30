6ec3db777c669c8359ed472c17cd692fbe6a1e92
frainerCalia <frainer.simarra@calia.ai>
2023-11-30 12:37:15 -0500

    config environment firebase

--- src/environment/enviroment.dev.ts
+++ src/environment/enviroment.dev.ts
@@ -1,22 +1,22 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCH7rA0dGHEkY-TOR0X3vRkREzH0ZcfcYQ",
-    authDomain: "db-menu-2fa2f.firebaseapp.com",
-    projectId: "db-menu-2fa2f",
-    storageBucket: "db-menu-2fa2f.appspot.com",
-    messagingSenderId: "918779377282",
-    appId: "1:918779377282:web:37cda1bfd4fe8b50e9669b",
-    measurementId: "G-05Z50E6B86"
+    apiKey: 'AIzaSyCH7rA0dGHEkY-TOR0X3vRkREzH0ZcfcYQ',
+    authDomain: 'db-menu-2fa2f.firebaseapp.com',
+    projectId: 'db-menu-2fa2f',
+    storageBucket: 'db-menu-2fa2f.appspot.com',
+    messagingSenderId: '918779377282',
+    appId: '1:918779377282:web:37cda1bfd4fe8b50e9669b',
+    measurementId: 'G-05Z50E6B86',
   },
   awsConfig: {
-    accessKeyId: "AKIA6JOEDBHZC5A76U75",
-    secretAccessKey: "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",
-    region: "us-east-1",
-    bucket: 'instagames-storage'
+    accessKeyId: 'AKIA6JOEDBHZC5A76U75',
+    secretAccessKey: '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',
+    region: 'us-east-1',
+    bucket: 'instagames-storage',
   },
-  url_site: 'http://localhost:4200/',
+  url_site: 'https://menu-qr-taupe.vercel.app/',
   title: 'Default Environment',
   nameCollection: 'product',
-  nameCollectionPayment: 'payment'
+  nameCollectionPayment: 'payment',
 };
